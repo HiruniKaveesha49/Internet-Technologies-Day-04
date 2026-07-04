6da56d73aa6e07e3c311cbb9679b7e41a86797eb
HiruniKaveesha49 <desilva-im23063@stu.kln.ac.lk>
2026-07-05 03:25:45 +0530

    Enhance index.html with improved layout, styling, and content for MXTX book showcase; add app.js and app.ts files for JavaScript and TypeScript examples.

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// console.log("Hello Java Script :)");
+
+// let name = "absfhsj"
+// console.log(name);
+// console.log(typeof name);
+// name = 'John Doe';
+// console.log(name);
+
+// let age = 25;
+// console.log(age);
+
+// alert("Hello World!");
+
+
+// let na='saman'
+// let a = 25;
+// let isStudent = true;
+// let customer = {}
+// let customers = []
+// let salary = 20000
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent);
+// console.log(typeof customer);
+// console.log(typeof customers);
+// console.log(typeof salary);
+
+// let x = 10;
+// let y = '10';
+
+// console.log(x===y);
+
---
+++ js/app.ts
@@ -0,0 +1,4 @@
+{
+    let name: string = "John Doe";
+    console.log(name);
+}
